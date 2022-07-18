9a78f77106b2d2d9327bc094f82a1a40aad95d65
Filipe <filipe@remote.com>
2022-07-18 14:42:01 +0100

    Clean up graphs

--- assets/js/hook/dashboard.js
+++ assets/js/hook/dashboard.js
@@ -2,8 +2,6 @@ import vegaEmbed from 'vega-embed'
 const Dashboard = {
   mounted() {
     this.handleEvent(`draw`, ({ spec }) => {
-      console.log(this.el)
-      console.log(spec)
       vegaEmbed(this.el, spec)
         .then((result) => result.view)
         .catch((error) => console.error(error))
