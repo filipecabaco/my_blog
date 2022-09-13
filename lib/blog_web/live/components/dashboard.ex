215a6e420e7bb1e66068f0ff5a6c2348e9ae18d8
Filipe <filipe@remote.com>
2022-09-13 12:43:52 +0100

    change graph direction

--- lib/blog_web/live/components/dashboard.ex
+++ lib/blog_web/live/components/dashboard.ex
@@ -9,14 +9,14 @@ defmodule BlogWeb.Components.Dashboard do
     metrics =
       Posts.list_post(nil)
       |> Enum.map(&String.replace(&1, ".md", ""))
-      |> Enum.map(fn title -> %{x: title, y: Statistics.fetch(title)} end)
+      |> Enum.map(fn title -> %{y: title, x: Statistics.fetch(title)} end)
 
     spec =
       VegaLite.new(title: "Visits", width: :container, height: :container, padding: 5)
       |> VegaLite.data_from_values(metrics)
       |> VegaLite.mark(:bar, color: "#98EED5")
-      |> VegaLite.encode_field(:x, "x", type: :nominal, title: "Title")
-      |> VegaLite.encode_field(:y, "y", type: :quantitative, title: "Views")
+      |> VegaLite.encode_field(:y, "y", type: :nominal, title: "Title")
+      |> VegaLite.encode_field(:x, "x", type: :quantitative, title: "Views")
       |> VegaLite.to_spec()
 
     {:ok, push_event(socket, "draw", %{"spec" => spec})}
@@ -25,7 +25,7 @@ defmodule BlogWeb.Components.Dashboard do
   @impl true
   def render(assigns) do
     ~H"""
-    <div style="width:80%; height: 500px" id="graph" phx-hook="Dashboard"/>
+    <div style="width: 80vw; height: 80vh" id="graph" phx-hook="Dashboard" />
     """
   end
 end
